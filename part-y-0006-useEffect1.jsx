0efeeee958f5f77505158a3f9c3c1debb5514a5b
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-12-15 11:53:44 +0300

    Create part-y-0006-useEffect1.jsx

---
+++ part-y-0006-useEffect1.jsx
@@ -0,0 +1,16 @@
+import { useEffect } from "react"
+
+function App() {
+
+  useEffect(() => {
+    console.log("Always, I'm here!")
+  })
+  
+  return (
+    <div>
+      hi there!
+    </div>
+  )
+}
+
+export default App
