7e85a9cb7cf451c68d388408e5582e5166237169
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-12-16 20:23:50 +0300

    Create part-y-0007-react-router3-App.jsx

---
+++ part-y-0007-react-router3-App.jsx
@@ -0,0 +1,23 @@
+import { Routes, Route } from "react-router-dom"
+
+import Home from "./pages/Home"
+import Products from "./pages/Products"
+import About from "./pages/About"
+
+function App() {
+
+  return (
+    <div>
+
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/home" element={<Home/>} />
+        <Route path="/products" element={<Products/>} />
+        <Route path="/about" element={<About/>} />
+      </Routes>
+
+    </div>
+  )
+}
+
+export default App
