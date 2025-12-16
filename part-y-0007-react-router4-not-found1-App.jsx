1a6294c973536586bdcc524be1bcd23e3fb45e2a
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-12-16 20:37:21 +0300

    Create part-y-0007-react-router4-not-found1-App.jsx

---
+++ part-y-0007-react-router4-not-found1-App.jsx
@@ -0,0 +1,27 @@
+import { Routes, Route } from "react-router-dom"
+
+import Home from "./pages/Home"
+import Products from "./pages/Products"
+import About from "./pages/About"
+
+import NotFound from "./pages/NotFound"
+
+function App() {
+
+  return (
+    <div>
+
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/home" element={<Home/>} />
+        <Route path="/products" element={<Products/>} />
+        <Route path="/about" element={<About/>} />
+
+        <Route path="*" element={<NotFound/>} />
+      </Routes>
+
+    </div>
+  )
+}
+
+export default App
