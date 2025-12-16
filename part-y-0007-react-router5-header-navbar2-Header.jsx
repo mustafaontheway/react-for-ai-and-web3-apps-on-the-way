2c9bc7cd7b630bf530ae359b5acb4cde02e775a8
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-12-16 20:46:13 +0300

    Create part-y-0007-react-router5-header-navbar2-Header.jsx

---
+++ part-y-0007-react-router5-header-navbar2-Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { Link } from 'react-router-dom'
+
+function Header() {
+  return (
+    <div>
+      <Link to={"/"}>Home</Link>
+      <Link to={"/products"}>Products</Link>
+      <Link to={"/about"}>About</Link>
+    </div>
+  )
+}
+
+export default Header
