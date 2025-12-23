1b11275f4424b45d1798711dac5493c56ad7521b
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-12-23 23:22:25 +0300

    Create part-y-0011-axios-delete1.jsx

---
+++ part-y-0011-axios-delete1.jsx
@@ -0,0 +1,57 @@
+import axios from "axios"
+import { useEffect } from "react"
+
+const BASE_URL = "http://localhost:3000"
+
+function App() {
+
+  const deleteProduct = async (id) => {
+
+    const response = await axios.delete(BASE_URL + "/inventory/" + id)
+
+    console.log(response.data)
+  }
+
+  const updateProduct = async (id, newProductData) => {
+
+    await axios.put(BASE_URL + "/inventory/" + id, newProductData)
+  }
+
+  const addProduct = async (newProduct) => {
+
+    await axios.post(BASE_URL + "/inventory", newProduct)
+  }
+
+  const getInventory = async () => {
+    
+    const response = await axios.get(BASE_URL + "/inventory")
+
+    console.log(response.data)
+  }
+
+  const getInventoryByID = async (id) => {
+
+    const response = await axios.get(BASE_URL + "/inventory/" + id)
+
+    console.log(response.data)
+  }
+
+  useEffect(() => {
+
+    deleteProduct("96")
+    //updateProduct(17, {"id": 17, "name": "iron", "quantity": 37, "per_price": 2780})
+    //addProduct({"id": 96, "name": "clock", "quantity": 65, "per_price": 650})
+    //getInventory()
+    //getInventoryByID(16)
+  }, [])
+
+  return (
+    <div>
+
+
+
+    </div>
+  )
+}
+
+export default App
